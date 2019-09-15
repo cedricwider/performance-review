e52a7f202c88e512010545a788d099e06f0d8e39
Cedric Wider <wider.cedric@gmail.com>
2019-09-15 20:31:52 +0200

    Tune nuxt proxy rewrite

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,8 +10,10 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-    link: [{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Fira+Sans|Lato&display=swap' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Fira+Sans|Lato&display=swap' },
+    ],
   },
   /*
    ** Customize the progress-bar color
@@ -26,9 +28,7 @@ export default {
    */
   proxy: {
     '/functions': {
-      target:
-        process.env.API_ENDPOINT ||
-        'https://performancereviews.azurewebsites.net/api/processReview?code=q1avJaJFhi8IXxJw/vPVC7K6aYezdCqUPteR0NMw5uCmXTgsukpcWQ==',
+      target: process.env.API_ENDPOINT || 'https://performancereviews.azurewebsites.net',
       pathRewrite: {
         '^/functions': '/api',
       },
@@ -60,4 +60,4 @@ export default {
      */
     extend(config, ctx) {},
   },
-}
+};
