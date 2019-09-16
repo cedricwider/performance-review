1144df3550789f4de20e8045b4f8efe1c1405bc0
Cedric Wider <wider.cedric@gmail.com>
2019-09-16 10:41:41 +0200

    Revert "Get rid of azure functions"
    This reverts commit 20847d9be539b704402a86cafcee259ea56e251f.

---
+++ functions/processReview/index.js
@@ -0,0 +1,58 @@
+const moment = require('moment')
+const dfs = require('dropbox-fs')({
+  apiKey: process.env.DROPBOX_ACCESS_TOKEN,
+})
+
+module.exports = async function(context, req) {
+  if (!req.body) {
+    return error(context)
+  }
+
+  const review = req.body
+  await dfs.writeFile(toFilename(review), toMarkup(review), { encoding: 'utf-8' }, () => {})
+  context.log('File uploaded!!!')
+}
+
+const error = context => {
+  context.res = {
+    status: 400,
+    body: JSON.stringify({ error: 'invalid request', message: 'Please provide a properly formatted request' }),
+  }
+}
+
+const toFilename = review => {
+  return `/performancereviews/${moment().format('YYYYMMDD')}_${review.firstName}_${review.lastName}.wiki`
+}
+
+const toMarkup = review => {
+  let markup = ''
+  markup += `= Performance Review ${review.firstName} ${review.lastName} =\n`
+  markup += `\n`
+  markup += `*Date:* ${moment().format('YYYY-MM-DD')}\n`
+  markup += '\n'
+  markup += '\n'
+  markup += '== Work Achievements ==\n'
+  markup += '\n'
+  review.achievements.forEach(achievement => (markup += `  * ${achievement}\n`))
+  markup += '\n'
+  markup += '\n'
+  markup += '== Behaviour ==\n'
+  markup += '\n'
+  ;(markup += `  * Communication: ${review.behaviour.communication}\n`),
+    (markup += `  * Execution: ${review.behaviour.execution}\n`),
+    (markup += `  * Implementation: ${review.behaviour.implementation}\n`),
+    (markup += `  * Teamwork: ${review.behaviour.teamwork}\n`),
+    (markup += `  * Innovation: ${review.behaviour.innovation}\n`),
+    (markup += '\n')
+  markup += '\n'
+  markup += '== Strengths ==\n'
+  markup += '\n'
+  review.strengths.forEach(strength => (markup += `  * ${strength}\n`))
+  markup += '\n'
+  markup += '\n'
+  markup += '== Opportunities ==\n'
+  markup += '\n'
+  review.opportunities.forEach(opportunity => (markup += `  * ${opportunity}\n`))
+
+  return markup
+}
